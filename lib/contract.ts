e3fd1629a9625a54ed97d7229d2625434456d587
MBThePro <131933737+MBThePro@users.noreply.github.com>
2025-11-14 16:57:26 +0700

    feat: add contract interactions with QuotationFactory and Quotation ABIs
    - Implemented public and wallet clients for interacting with the Sepolia network.
    - Added QuotationFactory and Quotation ABIs for contract interactions.
    - Set up factory address for deployed contracts.

---
+++ lib/contract.ts
@@ -0,0 +1,29 @@
+
+import QuotationFactoryAbi from '../frontend-abis/QuotationFactory.json';
+import QuotationAbi from '../frontend-abis/Quotation.json';
+import { createPublicClient, createWalletClient, custom, EIP1193Provider, http } from 'viem';
+import { sepolia } from 'wagmi/chains';
+
+// Your deployed factory address
+export const factoryAddress = '0xF4cE0722c8Ce62EcE3b92A3E88B4Ccfb913b04c8';
+
+// Infura key from environtment variables
+// const infuraKey = process.env.NEXT_PUBLIC_INFURA_API_KEY;
+
+// Public client (read-only)
+export const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(),
+    // transport: http(`https://sepolia.infura.io/v3/${infuraKey}`),
+});
+
+// Wallet client (write, requires user to connect wallet)
+export const walletClient = typeof window !== 'undefined' && (window as { ethereum?: EIP1193Provider }).ethereum
+    ? createWalletClient({
+        chain: sepolia,
+        transport: custom((window as { ethereum: EIP1193Provider }).ethereum),
+    })
+    : null;
+
+export const quotationFactoryAbi = QuotationFactoryAbi.abi;
+export const quotationAbi = QuotationAbi.abi;
