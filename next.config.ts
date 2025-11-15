553eb1c3343627d2322e147d2c471ae1f3edec59
MBThePro <131933737+MBThePro@users.noreply.github.com>
2025-11-15 22:31:47 +0700

    Update next.config.ts to add turbopack configuration and improve externals formatting; modify build script in package.json to use Webpack

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+
+  turbopack: {},
+
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
 };
